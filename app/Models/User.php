407036fe47c7664590e17b64a267981b0d735a3b
Mi Ninjaa <sachin@maishainfotech.com>
2023-08-28 11:45:10 +0530

    remove extra migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
+use Jenssegers\Mongodb\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
@@ -17,21 +18,28 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
+    /*
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
+    */
+
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
+    /*
     protected $hidden = [
         'password',
         'remember_token',
     ];
+    */
 
     /**
      * The attributes that should be cast.
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,9 +3,11 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 return new class extends Migration
 {
+    use DatabaseMigrations;
     /**
      * Run the migrations.
      *
@@ -15,7 +17,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
