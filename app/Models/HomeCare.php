b8ee03557b2e13a56bdd2beda16b5c29001a5aa8
Aditya Karmokar <adityakarmokar2000@gmail.com>
2023-11-09 18:14:36 +0530

    addy changes

---
+++ app/Models/HomeCare.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class HomeCare extends Model
+{
+    use HasFactory;
+    protected $connection = 'mongodb';
+    protected $collection = 'homeCare';
+    protected $fillable = [
+        "name",
+        "image",
+        "status",
+        "created_at",
+        "updated_at",
+        'trash'
+    ];
+}
